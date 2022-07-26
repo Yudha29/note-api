f59dd28ac88d86761502caabde8208a64395b2a0
Yudha Nawawi <yudhanawawi12@gmail.com>
2022-07-27 01:34:49 +0700

    initial commit

---
+++ src/test/java/com/github/yudha29/noteapi/NoteApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.yudha29.noteapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoteApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
